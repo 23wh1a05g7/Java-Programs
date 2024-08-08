5b67230be1eb9962d29e7e7f394efc3e090464df
23wh1a05g7 <154414713+23wh1a05g7@users.noreply.github.com>
2024-08-08 11:36:34 +0530

    Add files via upload

---
+++ createMultiObj.java
@@ -0,0 +1,12 @@
+package practice;
+
+public class createMultiObj {
+	double a = 55.6;
+	double b = 223.82;
+	public static void main(String[] args) {
+	createMultiObj obj1 = new createMultiObj();
+	createMultiObj obj2 = new createMultiObj();
+	System.out.println(obj1.a);
+	System.out.println(obj2.b);
+	}
+}
---
+++ createObj.java
@@ -0,0 +1,9 @@
+package practice;
+
+public class createObj {
+	int a = 2;
+	public static void main(String[] args) {
+	createObj obj1 = new createObj();
+	System.out.println(obj1.a);
+	}
+}
