5c956bad99c99ef3639268bab99a74f616e3c561
23wh1a05g7 <154414713+23wh1a05g7@users.noreply.github.com>
2024-08-24 15:33:08 +0530

    Add files via upload

---
+++ simpleInterest.java
@@ -0,0 +1,14 @@
+public class simpleInterest 
+{
+	public static void Main()
+	{
+		int p = 40000;
+		int t = 4;
+		int r = 1;
+		int result = (p*t*r) / 100;
+		System.out.println("Simple interest: " + result);
+	}
+	public static void main(String[] args) {
+		Main();
+	}
+}
