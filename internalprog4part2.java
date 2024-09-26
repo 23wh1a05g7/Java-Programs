48bbdf8e403670be9509ccf60ea4368161e3832c
23wh1a05g7 <154414713+23wh1a05g7@users.noreply.github.com>
2024-09-26 12:24:02 +0530

    Add files via upload

---
+++ internalprog4part2.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class GreatestNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter three numbers: ");
+        double num1 = scanner.nextDouble();
+        double num2 = scanner.nextDouble();
+        double num3 = scanner.nextDouble();
+
+        double greatest = num1;
+        if (num2 > greatest) {
+            greatest = num2;
+        }
+        if (num3 > greatest) {
+            greatest = num3;
+        }
+
+        System.out.println("The greatest number is: " + greatest);
+    }
+}
