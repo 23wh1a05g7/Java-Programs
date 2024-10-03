89b9fe6723615d3e5c5316cf1f0c4320091759c4
23wh1a05g7 <154414713+23wh1a05g7@users.noreply.github.com>
2024-10-03 12:38:57 +0530

    Add files via upload

---
+++ java_aws_prog1.java
@@ -0,0 +1,10 @@
+package java_awt_project;
+
+import java.awt.Frame;
+public class prog1 {
+public static void main(String[] args) {
+Frame f = new Frame ("My First Frame");
+f.setSize(400,300);
+f.setVisible(true);
+}
+}
